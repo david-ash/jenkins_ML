5d18e437c5dd02e56bb3abd0d08d31a11a92d97b
Ashish Singh <rsrdcr7@gmail.com>
2020-05-25 03:07:21 +0530

    Pushing the python code

--- counter.py
+++ counter.py
@@ -3,7 +3,6 @@ line = file.readlines()
 number=line[0][6:len(line[0])-1]
 number=int(number)+1
 number=str(number)
-print(number)
 line[0] = line[0][0:6] + number + line[0][len(line[0])-1]
 file = open('variables_file', 'w')
 file.writelines(line)
